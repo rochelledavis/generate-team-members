4ea8c890e38387742e19e3cfe2d599a260f03af8
rochelledavis <96760168+rochelledavis@users.noreply.github.com>
2022-04-03 11:31:53 -0600

    Employee Tests Complete

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,26 @@
+const Employee = require('../lib/Employee');
+
+test('creates an employee object', () => {
+    const employee = new Employee("rochelle", '123', 'me@me.com', 'Manager');
+    expect(employee.name).toEqual(expect.any(String));
+});
+
+test("create a name property", () => {
+    const employee = new Employee("rochelle", '123', 'me@me.com', 'Manager');
+    expect(employee.getName()).toEqual(expect.any(String));
+});
+
+test("create an ID property", () => {
+    const employee = new Employee("rochelle", '123', 'me@me.com', 'Manager');
+    expect(employee.getId()).toEqual(expect.any(String));
+});
+
+test("create an email property", () => {
+    const employee = new Employee("rochelle", '123', 'me@me.com', 'Manager');
+    expect(employee.getEmail()).toEqual(expect.any(String));
+});
+
+test("create an title property", () => {
+    const employee = new Employee("rochelle", '123', 'me@me.com', 'Manager');
+    expect(employee.getTitle()).toEqual(expect.any(String));
+});
